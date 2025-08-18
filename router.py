67e32584ab4d5da4e0ad2d19713aac767ee48ba5
Esteban <esteban.acevedo@qa-bit.com>
2025-08-17 22:11:28 -0400

    first changes

--- router.py
+++ router.py
@@ -6,7 +6,7 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
+#test
 service = Service(GeckoDriverManager().install())
 
 options = Options()
